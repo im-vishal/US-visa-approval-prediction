d98d74c0ed1eccd2742b8826c7dd11e0ad14a8bf
im-vishal <iamvirtualvishal@gmail.com>
2024-07-09 20:07:35 +0530

    Custom exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
-from us_visa import logger
+from us_visa.logger import logger
+from us_visa.exception import USvisaException
+import sys
 
-logger.info("Welcome to our custom logger.")
+# logger.info("Welcome to our custom logger.")
+
+
+try:
+    a = 2 / 0
+except Exception as e:
+    raise USvisaException(e, sys)
