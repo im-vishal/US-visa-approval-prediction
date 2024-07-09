ef6696f5b2e585e6038810114e43578d2f7efdaf
im-vishal <iamvirtualvishal@gmail.com>
2024-07-09 19:23:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="im-vishal",
+    author_email="iamvirtualvishal@gmail.com",
+    packages=find_packages()
+)
