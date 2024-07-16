64969ad8b07b50498d9f250b5ad2b2456ec1f152
im-vishal <iamvirtualvishal@gmail.com>
2024-07-16 21:54:37 +0530

    data_validation_added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -25,6 +25,11 @@ TEST_FILE_NAME: str = "test.csv"
 
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -35,3 +40,12 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR name
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
